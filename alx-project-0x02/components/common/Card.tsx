82acd9dfa912dd99c9c2793820e0e40a965ec80c
Emmanuel <emmanuelmark501@gmail.com>
2025-12-29 22:05:08 -0800

    creating a reusable card component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,13 +1,13 @@
-import {type CardProps } from '@/interfaces'
-import React from 'react'
+import { type CardProps } from "@/interfaces";
+import React from "react";
 
-function Card({title, content}: CardProps) {
+function Card({ title, content }: CardProps) {
   return (
     <div>
       <h2>{title}</h2>
       <p>{content}</p>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
