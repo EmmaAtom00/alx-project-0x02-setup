fd4f72931e11caacf040bbdef57d8142941d8561
Emmanuel <emmanuelmark501@gmail.com>
2025-12-30 00:21:01 -0800

    building a usercard component and getting familar with getStaticProps

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -21,6 +21,7 @@ const users: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
+
 export const getStaticProps = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
